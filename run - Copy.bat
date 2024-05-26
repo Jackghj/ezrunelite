6c18a401ff149b5e7bb3dc17596f0beaa67bd5b2
Jack <jackghj@gmail.com>
2024-05-26 22:03:47 +0200

    11 Plugins! and updated pluginstore

---
+++ run - Copy.bat
@@ -0,0 +1,1 @@
+java -ea -Drunelite.pluginhub.version=1.10.26.4 -cp net.runelite.client.Runelite -jar .\runelite-client\target\client-1.10.32-SNAPSHOT-shaded.jar --debug --developer-mode
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -ea -Drunelite.pluginhub.version=1.10.26.4 -cp net.runelite.client.Runelite -jar .\runelite-client\target\client-1.10.30-SNAPSHOT-shaded.jar --debug --developer-mode
+java -ea -Drunelite.pluginhub.version=1.10.31.1 -cp net.runelite.client.Runelite -jar .\runelite-client\target\client-1.10.32-SNAPSHOT-shaded.jar --debug --developer-mode
